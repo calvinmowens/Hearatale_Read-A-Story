fc180fc463a7c498eee40bc4e1b9b666bedc4206
Sungeun (Harry) Bae <43897776+harrysedev@users.noreply.github.com>
2021-09-20 16:30:22 -0400

    add audio player for story sound

---
+++ public/js/story.js
@@ -0,0 +1,12 @@
+myAudioObject.play()
+
+var myMusic = document.getElementById("story");
+
+function play() {
+    myMusic.play();
+}
+
+function pause() {
+    myMusic.pause();
+    prompt("test");
+}
