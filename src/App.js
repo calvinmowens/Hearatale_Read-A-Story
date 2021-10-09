354e4860b4f68f0fca2d3fe879375c32c8aefa2d
Sungeun (Harry) Bae <43897776+harrysedev@users.noreply.github.com>
2021-10-08 21:25:03 -0400

    convert Story into a component, made Popup component, update useState for Story

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
 import logo, { ReactComponent } from './logo.svg';
 import './App.css';
 import React from 'react';
-
+import Home from './Home'
+import Story from './Story';
+import { Route, Link } from 'react-router-dom';
 
 class App extends React.Component {
-  render (){
+  render() {
     return (
       <div className="App">
-        <section className="home_wrapper">
-        <img src={"img/WelcomeScreen.png"} className="home_backgroundImage" alt=""></img>
-            <div className="home_header">
-                <div className = "home_nav">
-                    <ul className="nav-area">
-                        <li className="flex1">{<a href="#">Read-A-Story</a>}}</li>
-                        <li className="flex2">{<a href="#">Brainy Phonics</a>}</li>
-                        <li className="flex3">{<a href="#">Brainy Sight Words</a>}</li>
-                        <li className="flex4">{<a href="#">Brainy Words 2000 </a>}</li>
-                        <li className="flex5">{<a href="#">Rhyme A Zoo</a>}</li>
-                    </ul>
-                </div>
-            </div>
-            <div className="home_article">
-                <a href="student-login.html" className="btn-student">Student Login</a>
-                <a href="#" className="btn-parent-teacher">Parent & Teacher Login</a>
-            </div>
-        </section>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/story" component={Story} />
       </div>
     );
   }
---
+++ src/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+import './Home.css';
+
+function Home() {
+    return (
+        <section onLoad={window['test']} className="home_wrapper">
+            <img src={"img/WelcomeScreen.png"} className="home_backgroundImage" alt=""></img>
+            <div className="home_header">
+                <div className="home_nav">
+                    <ul className="nav-area">
+                        <li className="flex1">{<a href="#">Read-A-Story</a>}</li>
+                        <li className="flex2">{<a href="#">Brainy Phonics</a>}</li>
+                        <li className="flex3">{<a href="#">Brainy Sight Words</a>}</li>
+                        <li className="flex4">{<a href="#">Brainy Words 2000 </a>}</li>
+                        <li className="flex5">{<a href="#">Rhyme A Zoo</a>}</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="home_article">
+                <a href="student-login.html" className="btn-student">Student Login</a>
+                <a href="#" className="btn-parent-teacher">Parent & Teacher Login</a>
+            </div>
+        </section>
+    );
+}
+
+export default Home;
