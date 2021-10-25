c65b621423a4b3ed9870222179c181e4950f7f85
sbae89 <sbae89@gatech.edu>
2021-10-25 17:22:16 -0400

    targetQuiz Update

--- src/component/TargetQuiz.js
+++ src/component/TargetQuiz.js
@@ -9,8 +9,16 @@ const TargetQuiz = ({ isQuizVisible, closeQuiz, targetWord }) => {
     return (isQuizVisible) ? (
         <div className="popup">
             <div className="popup-inner">
-                <div className="popupContainer">
-                    
+                <div className="popupContainer quizContainer">
+                <form>
+                    <h1 className="question">What is the definition of "WORD" ?</h1>
+                    <div className ="answerChoices">
+                        <div className="answerInput"><input id="answer" type="radio" name="group1" defaultValue="wrong" /> I am wrong answer.<br /></div>
+                        <div className="answerInput"><input id="answer" type="radio" name="group1" defaultValue="wrong" /> Also wrong answer<br /></div>
+                        <div className="answerInput"><input id="answer" type="radio" name="group1" defaultValue="correct" /> I am correct answer</div>
+                        <input className="turnin" type="submit" value="Turn In" ></input>
+                    </div>
+                </form>
                 </div>
                 <a id="closeBtn" className="close close-btn" onClick={closeQuiz}></a>
             </div>
