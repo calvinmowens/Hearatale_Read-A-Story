4e36fd1f61422a10e58f16670917316f56e57bc6
Sungeun (Harry) Bae <43897776+harrysedev@users.noreply.github.com>
2021-10-29 14:31:29 -0400

    add example json data and update target quiz

--- src/component/TargetQuiz.js
+++ src/component/TargetQuiz.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import './Popup.css';
 import './Quiz.css';
-import PostData from '../data/targetwords.json';
+import QuizData from '../data/exampleQuestions.json';
 
 
 const TargetQuiz = ({ isQuizVisible, closeQuiz, targetWord, quizzedWord }) => {
 
     console.log(quizzedWord);
-    const wordToFindInJson = PostData.filter(
+    const wordToFindInJson = QuizData.filter(
         (post) => post.word?.toLowerCase() === quizzedWord?.toLowerCase()
     )[0];
     console.log(wordToFindInJson);
@@ -16,15 +16,15 @@ const TargetQuiz = ({ isQuizVisible, closeQuiz, targetWord, quizzedWord }) => {
         <div className="popup">
             <div className="popup-inner">
                 <div className="popupContainer quizContainer">
-                <form>
-                    <h1 className="question">What is the definition of {targetWord} ?</h1>
-                    <div className ="answerChoices">
-                        <div className="answerInput"><input id="answer" type="radio" name="group1" defaultValue="wrong" /> I am wrong answer.<br /></div>
-                        <div className="answerInput"><input id="answer" type="radio" name="group1" defaultValue="wrong" /> Also wrong answer<br /></div>
-                        <div className="answerInput"><input id="answer" type="radio" name="group1" defaultValue="correct" /> I am correct answer</div>
-                        <input className="turnin" type="submit" value="Turn In" ></input>
-                    </div>
-                </form>
+                    <form>
+                        <h1 className="question">What is the definition of {targetWord} ?</h1>
+                        <div className="answerChoices">
+                            <div className="answerInput"><input id="answer" type="radio" name="group1" defaultValue="wrong" />{wordToFindInJson.qone}<br /></div>
+                            <div className="answerInput"><input id="answer" type="radio" name="group1" defaultValue="wrong" />{wordToFindInJson.qtwo}<br /></div>
+                            <div className="answerInput"><input id="answer" type="radio" name="group1" defaultValue="correct" />{wordToFindInJson.qthree}</div>
+                            <input className="turnin" type="submit" value="Turn In" ></input>
+                        </div>
+                    </form>
                 </div>
                 <a id="closeBtn" className="close close-btn" onClick={closeQuiz}></a>
             </div>
