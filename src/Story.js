e285c8ce1f4146ba79aa68d8b37fe5c7827facad
Calvin Owens <calvinowens@Jacks-MacBook-Pro.local>
2021-11-02 20:01:30 -0400

    added story lineBreak

--- src/Story.js
+++ src/Story.js
@@ -173,7 +173,7 @@ function Story() {
                                     {/* <div class="story-mid"> */}
                                     <div id="story-infinite-scroll">
                                         <p className="story-text">
-                                            { story.split(" ").map((ele, index) => (<span className="targetWord" onClick={handleWordClick}>{ele}</span>))}
+                                            { story.split(" ").map((ele, index) => (getTag(ele, handleWordClick)))}
                                             {/* <span className="targetWord" onClick={handleWordClick}>Once</span> upon a time there was a sweet little girl who was <span className="targetWord" onClick={handleWordClick}>loved</span> by everyone who knew her, but most of all by her grandmother, and there was nothing that she would not have given to the child. Once she gave the girl a little cape with a hood of red velvet, which suited her so well that she would never wear anything else; so she was alawys called 'Little Red Riding Hood.’ */}
                                         </p>
                                     </div>
@@ -194,6 +194,14 @@ function Story() {
         </div >
 
     );
+}
+
+function getTag(element, handleWordClick) {
+    if (element === '\n') {
+        return (<br></br>)
+    } else {
+        return (<span className="targetWord" onClick={handleWordClick}>{element}</span>)
+    }
 }
 
 export default Story;
