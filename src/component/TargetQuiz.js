60a036462a23753d1868873e12379945560ff884
Dabhi, Kush Dharmendra <kdabhi3@gatech.edu>
2021-11-04 23:10:33 -0400

    Added function that recognizes correct choices on quiz
    still need to add functionality that changes text color based on correct input

--- src/component/TargetQuiz.js
+++ src/component/TargetQuiz.js
@@ -12,6 +12,37 @@ const TargetQuiz = ({ isQuizVisible, closeQuiz, targetWord, quizzedWord }) => {
     )[0];
     console.log(wordToFindInJson);
 
+
+    const submitQuiz = (event) => {
+        console.log('testbykush');
+        //const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+        var radios = document.getElementsByTagName('input');
+        var value;
+        for (var i = 0; i < radios.length; i++) {
+            if (radios[i].type === 'radio' && radios[i].checked) {
+                // get value, set checked flag or do whatever you need to
+                value = radios[i].value;   
+                console.log('inside');    
+            }
+
+        }
+
+
+        if(value == "correct") {
+           console.log('works???');
+                //$("input[name=group1]").next().css('color', 'black')
+                //$("input[name=group]:checked").next().css('color', 'green');
+        } else {
+            console.log('doesnt works???');
+            console.log(value);
+
+        }
+        
+    };
+
+
+
+
     return (isQuizVisible) ? (
         <div className="popup">
             <div className="popup-inner">
@@ -20,11 +51,12 @@ const TargetQuiz = ({ isQuizVisible, closeQuiz, targetWord, quizzedWord }) => {
                     <form>
                         <h1 className="question">What is the definition of {targetWord} ?</h1>
                         <div className="answerChoices">
-                            <div className="answerInput"><input id="answer" className="radio" type="radio" name="group1" defaultValue="wrong" /><p>{wordToFindInJson.qone}</p></div>
-                            <div className="answerInput"><input id="answer" className="radio" type="radio" name="group1" defaultValue="wrong" /><p>{wordToFindInJson.qtwo}<br /></p></div>
-                            <div className="answerInput"><input id="answer" className="radio" type="radio" name="group1" defaultValue="correct" /><p>{wordToFindInJson.qthree}</p></div>
-                            <input className="turnin" type="submit" value="Submit" ></input>
+                            <div className="answerInput"><input id="answer" className="radio" type="radio" name="group1" defaultValue="wrong" /><p>{wordToFindInJson.wrongone}</p></div>
+                            <div className="answerInput"><input id="answer" className="radio" type="radio" name="group1" defaultValue="wrong" /><p>{wordToFindInJson.wrongtwo}<br /></p></div>
+                            <div className="answerInput"><input id="answer" className="radio" type="radio" name="group1" defaultValue="correct" /><p>{wordToFindInJson.correct}</p></div>
+                            
                         </div>
+                            <input className="turnin" type="button" onClick={submitQuiz} value="Submit" ></input>
                     </form>
                 </div>
                 <a id="closeBtn" className="close close-btn" onClick={closeQuiz}></a>
