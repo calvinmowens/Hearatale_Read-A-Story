34c193cc10bbffe58e3886f6f3b0ff8a4243acbb
sbae89 <sbae89@gatech.edu>
2021-11-17 13:20:48 -0500

    add hover event

---
+++ src/stories/goldilocks-and-the-three-bears/GoldilocksStoryQuiz.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+import '../Story.css';
+import Popup from "../component/Popup";
+//will need to change to a new json file.
+import PostData from '../data/targetwords.json'
+
+function Goldilocks() {
+
+
+    return (
+
+    )
+}
+
+
+
+export default Goldilocks;
